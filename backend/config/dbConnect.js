08ba6960505fc8a4fe94d007f1f057fbf122ea41
Swagat Talukdar <swagattalukdar007@gmail.com>
2024-02-06 23:30:42 +0530

    database URL updated

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = function dbConnect() {
   try {
-    mongoose.connect('mongodb://127.0.0.1:27017/shoepedia').then(result => console.log("successfully connected to DB"))
+    mongoose.connect('mongodb+srv://swagattalukdar007:bETH8coJ60tdV2VU@shoepedia.fkbdssy.mongodb.net/?retryWrites=true&w=majority').then(result => console.log("successfully connected to DB"))
   } catch (error) {
     console.log("Connection to DB failed");
   }
