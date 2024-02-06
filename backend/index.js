9fcae7c556b58ab640cb64abb8651c5095ded845
Swagat Talukdar <swagattalukdar007@gmail.com>
2024-02-06 23:42:25 +0530

    updated frontend URL

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use("/uploads", express.static(__dirname + "/uploads")); // Forgot to use this one
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:5173",
+  origin: "https://shoepedia.vercel.app",
 }));
 app.use(cookieParser());
 
